0e908cf9b787b990d86dc00326dac34df1553d24
Takeshi Nishio <kkeennnn+github@gmail.com>
2022-04-14 20:20:10 +0900

    キーマップ修正

--- Firmware/prk_nora/keymap.rb
+++ Firmware/prk_nora/keymap.rb
@@ -56,10 +56,10 @@ kbd.init_matrix_pins(
 # default layer should be added at first
 kbd.add_layer :default, %i(
   KC_ESC  KC_1    KC_2    KC_3    KC_4    KC_5    KC_KP_SLASH KC_KP_ASTERISK KC_KP_MINUS KC_KP_PLUS  KC_6    KC_7    KC_8    KC_9    KC_0    KC_EQL
-    KC_TAB  KC_Q    KC_W    KC_E    KC_R    KC_KP_7 KC_KP_8     KC_KP_9        KC_T        KC_Y        KC_U    KC_I    KC_O    KC_P    KC_BSPC
-    KC_LCTL KC_A    KC_S    KC_D    KC_F    KC_KP_4 KC_KP_5     KC_KP_6        KC_G        KC_H        KC_J    KC_K    KC_L    KC_MINS KC_ENT
-  KC_LSFT KC_Z    KC_X    KC_C    KC_V    KC_B    KC_KP_0     KC_KP_1        KC_KP_2     KC_KP_3     KC_N    KC_M    KC_COMM KC_DOT  KC_SLSH KC_RSFT
-                  KC_LALT       KC_LGUI     KC_SPC     KC_BSPC     KC_KP_0   KC_KP_DOT       KC_SPC  KC_RGUI  KC_RALT
+    KC_TAB  KC_Q    KC_W    KC_E    KC_R    KC_T    KC_KP_7     KC_KP_8     KC_KP_9        KC_Y        KC_U    KC_I    KC_O    KC_P    KC_BSPC
+    KC_LCTL KC_A    KC_S    KC_D    KC_F    KC_G    KC_KP_4     KC_KP_5     KC_KP_6        KC_H        KC_J    KC_K    KC_L    KC_MINS KC_ENT
+  KC_LSFT KC_Z    KC_X    KC_C    KC_V    KC_B    RGB_VAI     KC_KP_1        KC_KP_2     KC_KP_3     KC_N    KC_M    KC_COMM KC_DOT  KC_SLSH KC_RSFT
+                  KC_LALT       KC_LGUI     KC_SPC     RGB_VAD     KC_KP_0   KC_KP_DOT       KC_SPC  KC_RGUI  KC_RALT
 )
 
 rgb = RGB.new(
